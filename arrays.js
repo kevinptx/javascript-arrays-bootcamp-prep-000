b0c93d0ce9671a846ac41204be7de6a22b9f9fc1
Kevin Peery <kevin.peery@gmail.com>
2018-03-20 17:13:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,3 +2,7 @@ function addElementToBeginningOfArray(array, element){
   let newArray = [element, ...array]
   return newArray;
 }
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+}
