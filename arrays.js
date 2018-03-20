46db2864d8c3a21f40858a49a1990b3220b18953
Kevin Peery <kevin.peery@gmail.com>
2018-03-20 17:18:33 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,5 @@
+let chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
 function addElementToBeginningOfArray(array, element){
   let newArray = [element, ...array]
   return newArray;
@@ -12,3 +14,4 @@ function removeElementFromEndOfArray(array){
   array.slice(0, array.length-1)
   return array;
 }
+
