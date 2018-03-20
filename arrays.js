3cab096c0d03e6a6d0725eae13f99a9f7a7b190f
Kevin Peery <kevin.peery@gmail.com>
2018-03-20 18:20:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -35,7 +35,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  array = array.slice(1)
+  let array = array.slice(1)
   return array;
 }
 
